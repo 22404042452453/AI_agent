542b27ae197c6c0f15f77a5eba3d1adf501463e0
Kirill <89633993+22404042452453@users.noreply.github.com>
2025-11-12 21:01:26 +0300

    Delete test_docs.py

--- test_docs.py
+++
@@ -1,7 +0,0 @@
-import os
-from main import load_documents_from_directory
-
-docs = load_documents_from_directory('files')
-print(f'Найдено документов: {len(docs)}')
-for doc in docs:
-    print(f'Файл: {doc["metadata"]["filename"]}, Формат: {doc["metadata"]["format"]}, Длина: {len(doc["content"])} символов')
